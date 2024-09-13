896a73a92e9e0086720748605e19f54be81e1dd1
Gayathri Mahadevan <gayat_19@yahoo.com>
2024-09-13 14:28:40 +0530

    Day 8

---
+++ Day8/CollectionSolution/UnderstandingCollectionApp/Customer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingCollectionApp
+{
+    public class Customer : IEquatable<Customer>, IComparable<Customer>
+    {
+        public Customer()
+        {
+            
+        }
+        public Customer(int id, string name, long phone)
+        {
+            Id = id;
+            Name = name;
+            Phone = phone;
+        }
+        public void GetCustomerDetaislFromConsole()
+        {
+            Console.WriteLine("Enter the customer name");
+            Name = Console.ReadLine()??"";
+            Console.WriteLine("Enter the customer phone");
+            Phone = Convert.ToInt64(Console.ReadLine());
+        }
+        public int Id { get; set; }
+        public string Name{ get; set; } = string.Empty;
+        public long Phone { get; set; }
+        public override string ToString()
+        {
+            return "Id: " + Id + " Name: " + Name + " Phone: " + Phone;
+        }
+
+        public bool Equals(Customer? other)
+        {
+            Customer c1, c2;
+            c1 = this;
+            c2 = other;
+            if (c1.Id == c2.Id && c1.Name == c2.Name && c1.Phone == c2.Phone)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int CompareTo(Customer? other)
+        {
+            return this.Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ Day8/CollectionSolution/UnderstandingCollectionApp/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+
+namespace UnderstandingCollectionApp
+{
+    internal class Program
+    {
+        void UnderstandingCollection()
+        {
+            ArrayList numbers = new ArrayList();
+            numbers.Add(100);
+            numbers.Add(234);
+            numbers.Add(new Random().Next(100, 200));
+            numbers.Add(new Random().Next(100, 200));
+            numbers.Add(new Random().Next(100, 200));
+            numbers.Add(new Random().Next(100, 200));
+            numbers.Add("Hello");
+            int sum = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum += Convert.ToInt32(numbers[i]);   
+            }
+            Console.WriteLine(sum);
+        }
+        void UnderstandingList()
+        {
+            List<int> numbers = new List<int>();//generic collection - typesafe
+            numbers.Add(100);
+            //numbers.Add("Hello");//not possible
+            for(int i = 0; i < 10; i++)
+            {
+                numbers.Add(new Random().Next(i, 100));
+            }
+            numbers.Sort();
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        void UnderstaingMoreOnList()
+        {
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer(100,"Ramu",1234567890),
+                new Customer(101,"Bimu",5544332211),
+                new Customer(102,"Domu",9876543210 )
+            };
+            //int choice = 0;
+            //do
+            //{
+            //    Customer customer = new Customer();
+            //    customer.GetCustomerDetaislFromConsole();
+            //    customer.Id = customers.Count + 100;
+            //    customers.Add(customer);    
+            //    Console.WriteLine("Do you want to continue? Then enter any number otehr than 0.");
+            //    choice = Convert.ToInt32(Console.ReadLine());
+            //} while (choice !=0);
+            Console.WriteLine("----------------------------------------");
+
+            //foreach (var customer in customers)
+            //{
+            //    Console.WriteLine(customer);
+            //}   
+            //Console.WriteLine(customers[0]);//We can access the elements using index
+            //bool isCustomerFound = customers.Contains(new Customer(100, "Ramu", 1234567890));
+            //Console.WriteLine("Is Ramu present "+isCustomerFound);
+            var index = customers.IndexOf(new Customer(100, "Ramu", 1234567890));
+            Console.WriteLine(index);
+            //customers.RemoveAt(index);
+            //customers.Remove(new Customer(100, "Ramu", 1234567890));
+            customers.Sort();
+            foreach (var item in customers)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        void UnderstandingDictionary()
+        {
+            Dictionary<int, Customer> customers = new Dictionary<int, Customer>();
+            customers.Add(100, new Customer(100, "Ramu", 1234567890));
+            customers.Add(101, new Customer(101, "Bimu", 5544332211));
+            customers.Add(101, new Customer(101, "Domu", 9876543210));
+            foreach (var item in customers.Keys)
+            {
+                Console.WriteLine(customers[item]);
+            }
+        }
+            void UnderstaningLimitationOfArray()
+        {
+            int[] numbers = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                numbers[i] = i *100+ new Random().Next(10,100);
+            }
+            //To increase the size of array we have to create a new array and copy the old array to new array
+            int[] nums1 = new int[12];
+            for (int i = 0; i < 10; i++)
+            {
+                nums1[i] = numbers[i];
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+           // program.UnderstaningLimitationOfArray();
+           //program.UnderstandingCollection();
+           //program.UnderstandingList();
+          // program.UnderstaingMoreOnList();
+                program.UnderstandingDictionary();
+        }
+    }
+}
